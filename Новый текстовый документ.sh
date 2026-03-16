dabbae3af93b30b6a128776835ad1153dd3a2caa
compressor333 <compressor333@gmail.com>
2026-03-16 13:24:04 +0300

    initial commit

--- Новый текстовый документ.sh
+++ Новый текстовый документ.sh
@@ -2,4 +2,4 @@ git init &&
 git add . &&
 git commit -m "initial commit" && 
 git remote add origin https://github.com/compressor333/clinic.git &&
-git push -u origin main
+git push -u origin master
